b3a9c4f0180c2dbd18b5fe9e50a3ce3eda141721
RUBEN PEÑA PARADA <46791460+rubenpenap@users.noreply.github.com>
2025-07-07 17:39:42 -0400

    refactor: standardize string quotes in getServerSideProps function and related tests

--- src/04-conditional-types-and-infer/26-get-result-from-async-function.problem.ts
+++ src/04-conditional-types-and-infer/26-get-result-from-async-function.problem.ts
@@ -1,22 +1,26 @@
-import { Equal, Expect } from "../helpers/type-utils";
+import { Equal, Expect } from '../helpers/type-utils';
 
 const getServerSideProps = async () => {
-  const data = await fetch("https://jsonplaceholder.typicode.com/todos/1");
-  const json: { title: string } = await data.json();
-  return {
-    props: {
-      json,
-    },
-  };
+	const data = await fetch('https://jsonplaceholder.typicode.com/todos/1');
+	const json: { title: string } = await data.json();
+	return {
+		props: {
+			json,
+		},
+	};
 };
 
-type InferPropsFromServerSideFunction = unknown;
+type InferPropsFromServerSideFunction<T> = T extends () => Promise<{
+	props: infer P;
+}>
+	? P
+	: never;
 
 type tests = [
-  Expect<
-    Equal<
-      InferPropsFromServerSideFunction<typeof getServerSideProps>,
-      { json: { title: string } }
-    >
-  >
+	Expect<
+		Equal<
+			InferPropsFromServerSideFunction<typeof getServerSideProps>,
+			{ json: { title: string } }
+		>
+	>,
 ];
