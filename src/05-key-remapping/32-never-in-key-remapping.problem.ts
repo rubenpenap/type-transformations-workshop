937a7f8a82fdad21eabd8faefbf8b7bd254d1117
RUBEN PEÑA PARADA <46791460+rubenpenap@users.noreply.github.com>
2025-07-09 15:35:37 -0400

    refactor: standardize string quotes in never-in-key-remapping problem type definition and related tests

--- src/05-key-remapping/32-never-in-key-remapping.problem.ts
+++ src/05-key-remapping/32-never-in-key-remapping.problem.ts
@@ -1,25 +1,29 @@
-import { Equal, Expect } from "../helpers/type-utils";
+import { Equal, Expect } from '../helpers/type-utils';
 
 interface Example {
-  name: string;
-  age: number;
-  id: string;
-  organisationId: string;
-  groupId: string;
+	name: string;
+	age: number;
+	id: string;
+	organisationId: string;
+	groupId: string;
 }
 
-type OnlyIdKeys<T> = unknown;
+type OnlyIdKeys<T> = {
+	[K in keyof T as K extends `${string}${'id' | 'Id'}${string}`
+		? K
+		: never]: T[K];
+};
 
 type tests = [
-  Expect<
-    Equal<
-      OnlyIdKeys<Example>,
-      {
-        id: string;
-        organisationId: string;
-        groupId: string;
-      }
-    >
-  >,
-  Expect<Equal<OnlyIdKeys<{}>, {}>>
+	Expect<
+		Equal<
+			OnlyIdKeys<Example>,
+			{
+				id: string;
+				organisationId: string;
+				groupId: string;
+			}
+		>
+	>,
+	Expect<Equal<OnlyIdKeys<{}>, {}>>,
 ];
